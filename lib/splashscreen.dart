78e457d377cf3eba22e17c7786aefb3c6637fadb
ItaloSSilva <italo.s.silva@rede.ulbra.br>
2022-07-18 12:04:44 -0300

    home created and connected with splashscreen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
+import 'package:gato_cachorro/home.dart';
 
 class MySplash extends StatefulWidget {
   const MySplash({Key? key}) : super(key: key);
@@ -16,10 +16,11 @@ class _MySplashState extends State<MySplash> {
   Widget build(BuildContext context) {
     return SplashScreen(
         seconds: 2,
-        title: Text('Classificador de gatos e cães',
+        navigateAfterSeconds: Home(),
+        title: const Text('Classificador de gatos e cães',
             style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 25,
-                color: Color(0x00FFFF))));
+                color: Color(0x0000ffff))));
   }
 }
