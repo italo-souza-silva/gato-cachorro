3f1da7a425f5492baa6f274ace3ebb9e3468ad2a
ItaloSSilva <italo.s.silva@rede.ulbra.br>
2022-07-18 12:01:15 -0300

    splashscreen configurated

---
+++ lib/splashscreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplash extends StatefulWidget {
+  const MySplash({Key? key}) : super(key: key);
+
+  @override
+  State<MySplash> createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+        seconds: 2,
+        title: Text('Classificador de gatos e cães',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+                color: Color(0x00FFFF))));
+  }
+}
